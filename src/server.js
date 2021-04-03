2eb5c6a816c57127b4b6a66810b7944eb4eb1033
Fabio Teraoka <65736019+fabioteraoka@users.noreply.github.com>
2021-04-03 20:33:31 -0300

    outro teste

--- src/server.js
+++ src/server.js
@@ -18,5 +18,8 @@ server.use(express.urlencoded({extended:true}))
 server.use(routes)
 //server.listen(3000, () => console.log ('rodando')) //posso fazer forma longa server.listen (3000, function(){console.log('rodando')}) ¡
 
-var porta = process.env.PORT || 8080 
-server.listen(porta, () => console.log('rodando'))
+//var porta = process.env.PORT || 8080 
+//server.listen(porta, () => console.log('rodando'))
+
+var porta = process.env.PORT || 8080;
+app.listen(porta);
